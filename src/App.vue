0f9d2cef6b6b7c1d6eb19da3bced5fa2f5fada80
Ngozi <124025329+fierygriffin@users.noreply.github.com>
2023-02-24 21:43:56 +0100

    Project Initialization

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex w-full h-screen">
+    <h1 class="text-5xl font-OpenSans text-emerald-600 text-center m-auto">Weather App with Vue 3</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+    fontFamily: {
+      OpenSans: ["'Open Sans', sans-serif"],
+    },
+  },
+  plugins: [],
+}
