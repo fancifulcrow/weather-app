6a22fcb8762a654de0375d2d1779224d65f75234
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2024-02-29 20:47:06 +0100

    Update CityView.vue

--- src/components/CityView.vue
+++ src/components/CityView.vue
@@ -1,24 +1,7 @@
 <template>
-    <!-- Card for the weather at a location -->
-    <div class="rounded-xl w-full text-center p-8 border-black border-solid border-2">
-        <div class="flex flex-col text-left">
-            <span><i class="fa-solid fa-location-dot"></i> Berlin, Germany</span>
-            <span>12:34</span>
-        </div>
-        
-        <div class="flex items-center justify-between">
-            <div class="flex flex-col text-left justify-start">
-                <span class="text-5xl">30</span>
-                <span>Slightly Cloudy</span>
-            </div>
-            <div>
-                <div class="w-24 h-24">
-                    <img src="../assets/cloudy.png" class="w-full h-full">
-                </div>
-            </div>
-        </div>
+    <div class="flex justify-center items-center">
+        <span class="text-5xl">7</span>
     </div>
-
 </template>
 
 <script setup>
