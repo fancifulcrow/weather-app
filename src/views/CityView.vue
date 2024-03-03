76c72e7b4b17971660e631fce2349f399e7f4e7d
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2024-03-03 22:25:33 +0100

    use Open-Meteo for geocoding

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-
+    <div class="container max-w-2xl">
+        <Suspense>
+            <AsyncCityView/>
+            <template #fallback>
+                <p>loading...</p>
+            </template>
+        </Suspense>
     </div>
 </template>
 
 <script setup>
+import AsyncCityView from '@/components/AsyncCityView.vue';
+
 
 </script>
